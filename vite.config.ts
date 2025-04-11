058986522c51874088701df06f6993f6e14ea4a6
Bernardo Bossi <bernardobossi77@gmail.com>
2025-04-11 16:58:16 +0100

    feat: update vite file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      external: ['@ant-design/icons'],
+    },
+  },
   plugins: [react()],
-})
+});
