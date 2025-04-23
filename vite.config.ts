55b3e909875c59b6bb3c78444ecf221b1f1d4a49
fullergalway <1871541+fullergalway@users.noreply.github.com>
2025-04-23 13:18:58 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
   },
-  base: `/apps/${process.env.OS_DEPLOYMENT_NAME}/`,
+  base: process.env.OS_BASE_PATH,
   plugins: [react()],
 });
